a24af9e73a199c532367fd18509b19e6f41314aa
ProfessorBlake <cblake@qu.edu>
2021-04-06 11:20:52 -0400

    Initial commit

---
+++ Assets/6.0 Intro/Scripts/CameraRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public Transform Pivot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pivot.Rotate(Vector3.up, Input.GetAxis("Mouse X") * 3f);
+    }
+}
---
+++ Assets/6.5 Movement/Scripts/FreeMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMove : MonoBehaviour
+{
+    public Transform Parent;
+    void Update()
+    {
+        Parent.transform.Rotate(new Vector3(0f,Input.GetAxis("Mouse X")) * 5f);
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0) * 5f);
+        Parent.transform.position += transform.forward * Input.GetAxis("Vertical");
+        Parent.transform.position += transform.right * Input.GetAxis("Horizontal");
+    }
+}
