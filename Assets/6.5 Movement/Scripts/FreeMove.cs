b33ae5ceee631932eecfd3accb83c7d4794f849f
ProfessorBlake <cblake@qu.edu>
2021-04-07 16:23:53 -0400

    Movement changes

--- Assets/6.5 Movement/Scripts/FreeMove.cs
+++ Assets/6.5 Movement/Scripts/FreeMove.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class FreeMove : MonoBehaviour
 {
     public Transform Parent;
+    public float LookSpeed;
+    public float MoveSpeed;
     void Update()
     {
-        Parent.transform.Rotate(new Vector3(0f,Input.GetAxis("Mouse X")) * 5f);
-        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0) * 5f);
-        Parent.transform.position += transform.forward * Input.GetAxis("Vertical");
-        Parent.transform.position += transform.right * Input.GetAxis("Horizontal");
+        Parent.transform.Rotate(new Vector3(0f,Input.GetAxis("Mouse X")) * LookSpeed);
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0) * LookSpeed);
+        Parent.transform.position += transform.forward * Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+        Parent.transform.position += transform.right * Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
     }
 }
